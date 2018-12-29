2c3adb30e1a0e707a553b9c1d6c731ccd863da3e
Rishabh Jain <mailrishabh1403@gmail.com>
2018-12-29 14:31:09 +0530

    add build

---
+++ precache-manifest.586282fc9c7b5f46864fb012b35e1911.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "eeb6c00b6c8506b9e494",
+    "url": "/static/js/main.eeb6c00b.chunk.js"
+  },
+  {
+    "revision": "64b35ba1cb96cc7c5a88",
+    "url": "/static/js/1.64b35ba1.chunk.js"
+  },
+  {
+    "revision": "eeb6c00b6c8506b9e494",
+    "url": "/static/css/main.bc4c5e67.chunk.css"
+  },
+  {
+    "revision": "beb21fe316b5935f3014a790c9efe5ec",
+    "url": "/index.html"
+  }
+];
